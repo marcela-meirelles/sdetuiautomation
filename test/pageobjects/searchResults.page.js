9ab68162e9c79278e7ce6c8c9f6c1b5871587f68
Marcela Meirelles <marcemei@gmail.com>
2020-12-19 19:28:41 -0300

    Fix pagination selectors and adds navigation to specific page number method

--- test/pageobjects/searchResults.page.js
+++ test/pageobjects/searchResults.page.js
@@ -4,28 +4,33 @@ const Page = require('./page');
  * sub page containing specific selectors and methods for a specific page
  */
 class SearchResults extends Page {
+    
     /**
      * define selectors using getter methods
      */
-    get resultsTable () { return $('#####') }
-    get resultsPageNumber () { return $('####') }
-    get resultsPagination () { return $('#####') }
-    // get btnNavToPageN (pageNumber) { return $('button[type="submit"]') }
+    get resultsList () { return $('.list-items') }
+    get resultsListElements() { return $$('li .list-item')}  
+    get resultsPagination () { return $('.list-pagination') }
+    get resultsFirstPage () { return $('//div[@class=\'list-pagination\']//button[contains(@class, \'next-current\')]') }
 
     /**
-     * a method to encapsule automation code to interact with this page object
-     * 
+     * overwrite open method to navigate to search page with iPhone value as searchText
+     * in the future the searchText value could be parametrized and passed from a config file
+     */
+    open(){
+        return super.open(`wholesale?catId=0&SearchText=iPhone`);
+    }
+
+    /**
+     * navigates to a specific page number available in the pagination area at the bottom  
+     * of the search results. The pageNumber needs to be enabled for the method to work
      */
     navigateToSpecificPage (pageNumber) {
-        this.btnNavToPageN(pageNumber).click(); 
+        this.resultsPagination.scrollIntoView();
+        $(`//div[@class='list-pagination']//button[contains(text(), '${pageNumber}')]`).waitForEnabled(2000);
+        $(`//div[@class='list-pagination']//button[contains(text(), '${pageNumber}')]`).click(); 
     }
-
-    /**
-     * overwrite specifc options to adapt it to page object
-     */
-    open () {
-        return super.open('');
-    }
+ 
 }
 
 module.exports = new SearchResults();
