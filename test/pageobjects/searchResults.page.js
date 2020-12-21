7ee7baf21be091abb85900db30d1545deb58d60c
Marcela Meirelles <marcemei@gmail.com>
2020-12-21 05:20:10 -0300

    Adds methods to handle any item listed

--- test/pageobjects/searchResults.page.js
+++ test/pageobjects/searchResults.page.js
@@ -1,36 +1,58 @@
 const Page = require('./page');
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class SearchResults extends Page {
     
     /**
-     * define selectors using getter methods
+     * Define selectors using getter methods
      */
     get resultsList () { return $('.list-items') }
     get resultsListElements() { return $$('li .list-item')}  
     get resultsPagination () { return $('.list-pagination') }
-    get resultsFirstPage () { return $('//div[@class=\'list-pagination\']//button[contains(@class, \'next-current\')]') }
+    get resultsCurrentPage () { return $('//div[@class=\'list-pagination\']//button[contains(@class, \'next-current\')]') }
 
     /**
-     * overwrite open method to navigate to search page with iPhone value as searchText
-     * in the future the searchText value could be parametrized and passed from a config file
+     * Overwrite open method to navigate to search page with iPhone value as searchText.
+     * In the future the searchText value could be parametrized and passed from a config file
      */
     open(){
         return super.open(`wholesale?catId=0&SearchText=iPhone`);
     }
 
     /**
-     * navigates to a specific page number available in the pagination area at the bottom  
-     * of the search results. The pageNumber needs to be enabled for the method to work
+     * Navigates to a specific page number available in the pagination area at the bottom  
+     * of the search results. 
      */
     navigateToSpecificPage (pageNumber) {
         this.resultsPagination.scrollIntoView();
         $(`//div[@class='list-pagination']//button[contains(text(), '${pageNumber}')]`).waitForEnabled(2000);
         $(`//div[@class='list-pagination']//button[contains(text(), '${pageNumber}')]`).click(); 
     }
+
+    /**
+     * Pass the number in which the item is placed starting to count from one onwards.
+     * Each listed item has an attribute "product-index" which value starts from zero and continues to add 1
+     * until the last listed item in the page. For another page, the product-index starts again.
+     * 
+     * This index is NOT the DOM element index. Getting elements by DOM index is a practice we firmly discourage.
+     * @param {*} placeNumber 
+     */
+    getItemByListingPlace(placeNumber){
+        placeNumber=placeNumber-1; //we will grab the item by the attr product-index which works as an array index (starting at 0)
+        $(`//li[@class='list-item']//div[contains(@product-index, '${placeNumber}')]`).waitForDisplayed(2000);
+        return $(`//li[@class='list-item']//div[contains(@product-index, '${placeNumber}')]`);
+    }
+
+    navigateToItem(item){
+        item.waitForEnabled(2000);
+        item.scrollIntoView();
+        item.click();
+    }
  
+    navigateToItemByListingPlace(placeNumber){
+        let item = this.getItemByListingPlace(--placeNumber);
+        this.navigateToItem(item);
+    }
+
 }
 
 module.exports = new SearchResults();
